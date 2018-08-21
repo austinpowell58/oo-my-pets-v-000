a170b67c5d1e0e74cc3807bbf6f9514e73b42bb6
Austin Powell <austinpowell58@gmail.com>
2018-08-21 17:21:45 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,7 +3,7 @@ class Owner
   attr_accessor :pets
   
   def initialize
-    @pets = {fishes: [], cats: [], dogs: []}
+    @pets = {:fishes =>[], :cats => [], :dogs []}
   end
   
   def buy_cat(name_cat)
